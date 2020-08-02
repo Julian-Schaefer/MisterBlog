ef92025cffcf5e510d2c588130bd77f14a3367f0
Julian Schäfer <julian.david.schaefer@gmail.com>
2020-08-02 16:36:58 +0200

    Borders on Hover

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -13,6 +13,14 @@ export class AppComponent {
     console.log(e.target.innerText);
   }
   
+  onMouseOver(e: any) {
+    e.target.style.border = "black 1px solid";
+  }
+  
+  onMouseOut(e: any) {
+    e.target.style.border = "none";
+  }
+  
   constructor(private http: HttpClient) { 
     http.get("http://localhost:8080/hello/html", {responseType: 'text'}).subscribe((data) => this.comment = data);
   }
