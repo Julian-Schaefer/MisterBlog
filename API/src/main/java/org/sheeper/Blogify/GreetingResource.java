b8e5617dada69ba76f7bb4adc769c272611dc9a7
Julian Schäfer <julian.david.schaefer@gmail.com>
2020-08-02 14:18:43 +0200

    Initial Commit

---
+++ API/src/main/java/org/sheeper/Blogify/GreetingResource.java
@@ -0,0 +1,45 @@
+package org.sheeper.Blogify;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Article> hello() {
+        try {
+            Document doc = Jsoup.connect("https://navbitsbytes.com").get();
+            var articleElements = doc.select("article.post");
+            if (articleElements.isEmpty()) {
+                articleElements = doc.select("div.post");
+            }
+            List<Article> articles = new LinkedList<>();
+            for (var articleElement : articleElements) {
+                Article article = new Article();
+                article.setTitle(articleElement.select(".entry-title").first().text());
+                article.setIntroduction(articleElement.select(".entry-content").first().children().first().text());
+                article.setContent(articleElement.select(".entry-title").first().text());
+                articles.add(article);
+            }
+
+            return articles;
+        } catch (
+
+        IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
