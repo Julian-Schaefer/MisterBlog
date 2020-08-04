44dbc4d11d6641c6f3f0f4db3784370b6c01ea8d
Julian Schäfer <julian.david.schaefer@gmail.com>
2020-08-04 16:05:10 +0200

    Selection Process

--- Frontend/src/app/preview/preview.component.ts
+++ Frontend/src/app/preview/preview.component.ts
@@ -7,6 +7,7 @@ interface IArticleSelection {
   dateSelector?: string;
   authorSelector?: string;
   introductionSelector?: string;
+  contentSelector?: string;
 }
 
 @Component({
@@ -17,14 +18,14 @@ interface IArticleSelection {
 export class PreviewComponent {
 
   step = 1;
-  comment = '';
-  articleSelection: IArticleSelection;
+  previewHtml: string;
   selectedElement: any;
   selectedElements: any[] = [];
+  nextButtonEnabled = false;
 
   constructor(private http: HttpClient, public activatedRoute: ActivatedRoute) {
     let url = this.activatedRoute.snapshot.queryParamMap.get('url');
-    http.get("http://localhost:8080/html?url=" + url, { responseType: 'text' }).subscribe((data) => this.comment = data);
+    http.get("http://localhost:8080/html?url=" + url, { responseType: 'text' }).subscribe((data) => this.previewHtml = data);
   }
 
   onElementSelected(e: any) {
@@ -34,34 +35,17 @@ export class PreviewComponent {
 
     this.selectedElement = e.target;
     this.selectedElement.style.border = "red 2px solid";
-    console.log(this.articleSelection);
+    this.nextButtonEnabled = true;
   }
 
   onNextClick(): void {
+    this.nextButtonEnabled = false;
+
     if (this.selectedElement) {
       this.selectedElement.style.border = "none";
     }
 
-    let selected = this.selectedElement;
-    let selectorString = this.getTagName(selected);
-    while (selected.parentElement) {
-      if (selected.parentElement.tagName.toLowerCase() === "div" && selected.parentElement.id === "preview-div") {
-        break;
-      }
-
-      selectorString = this.getTagName(selected.parentElement) + selectorString;
-      selected = selected.parentElement;
-    }
-
-    this.selectedElements[this.step] = this.selectedElement;
-
-    // switch (this.step) {
-    //   case 1: this.articleSelection = { ...this.articleSelection, headerSelector: selectorString }; break;
-    //   case 2: this.articleSelection = { ...this.articleSelection, dateSelector: selectorString }; break;
-    //   case 3: this.articleSelection = { ...this.articleSelection, authorSelector: selectorString }; break;
-    //   case 4: this.articleSelection = { ...this.articleSelection, introductionSelector: selectorString }; break;
-    // }
-
+    this.selectedElements[this.step - 1] = this.selectedElement;
     this.step += 1;
   }
 
@@ -72,11 +56,51 @@ export class PreviewComponent {
 
     this.step -= 1;
 
-    this.selectedElement = this.selectedElements[this.step];
+    this.selectedElement = this.selectedElements[this.step - 1];
     this.selectedElement.style.border = "red 2px solid";
   }
 
-  getTagName(element: any) {
+  onFinishClick(): void {
+    if (this.selectedElement) {
+      this.selectedElement.style.border = "none";
+    }
+
+    this.selectedElements[this.step - 1] = this.selectedElement;
+
+    let counter = 1;
+    let articleSelection: IArticleSelection;
+
+    for (let element of this.selectedElements) {
+      let selected = element;
+      let selectorString = this.getTagName(selected);
+      while (selected.parentElement) {
+        if (selected.parentElement.tagName.toLowerCase() === "div" && selected.parentElement.id === "preview-div") {
+          break;
+        }
+
+        selectorString = this.getTagName(selected.parentElement) + selectorString;
+        selected = selected.parentElement;
+      }
+
+      selectorString = selectorString.trim();
+
+      switch (counter) {
+        case 1: articleSelection = { ...articleSelection, headerSelector: selectorString }; break;
+        case 2: articleSelection = { ...articleSelection, dateSelector: selectorString }; break;
+        case 3: articleSelection = { ...articleSelection, authorSelector: selectorString }; break;
+        case 4: articleSelection = { ...articleSelection, introductionSelector: selectorString }; break;
+        case 5: articleSelection = { ...articleSelection, contentSelector: selectorString }; break;
+      }
+
+      counter++;
+    }
+
+    this.http.post("http://localhost:8080/blog-selection", articleSelection).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  getTagName(element: any): string {
     return element.tagName.toLowerCase() + ' ';
   }
 
