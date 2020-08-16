cd33ecea911c9ef0f80b31d88ea3a726971b10fa
Julian Schäfer <julian.david.schaefer@gmail.com>
2020-08-16 20:03:20 +0200

    Renamed to BlogPost

---
+++ API/src/main/java/org/sheeper/Blogify/BlogPost.java
@@ -0,0 +1,54 @@
+package org.sheeper.Blogify;
+
+public class BlogPost {
+
+    private String title;
+    private String date;
+    private String author;
+    private String introduction;
+    private String content;
+
+    public BlogPost() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
