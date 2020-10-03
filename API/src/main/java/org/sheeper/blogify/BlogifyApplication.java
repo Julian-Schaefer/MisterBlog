985a4170729523d8b19ef5db385fb81e57abcff0
Julian Schäfer <julian.david.schaefer@gmail.com>
2020-10-03 12:23:38 +0200

    Multiple Environments

--- API/src/main/java/org/sheeper/blogify/BlogifyApplication.java
+++ API/src/main/java/org/sheeper/blogify/BlogifyApplication.java
@@ -3,11 +3,13 @@ package org.sheeper.blogify;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -15,6 +17,12 @@ import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class BlogifyApplication {
+
+	@Value("${firebase.location}")
+	private String firebaseLocation;
+
+	@Value("${firebase.url}")
+	private String firebaseDatabaseUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogifyApplication.class, args);
@@ -26,12 +34,22 @@ public class BlogifyApplication {
 			FirebaseApp.getInstance();
 		} catch (IllegalStateException ex) {
 			try {
-				String serviceAccountString = System.getenv("FIREBASE");
-				InputStream serviceAccount = new ByteArrayInputStream(serviceAccountString.getBytes());
+				InputStream serviceAccount = null;
+
+				if (firebaseLocation != null) {
+					serviceAccount = new FileInputStream(firebaseLocation);
+				} else {
+					String serviceAccountString = System.getenv("FIREBASE");
+					serviceAccount = new ByteArrayInputStream(serviceAccountString.getBytes());
+				}
+
+				if (firebaseDatabaseUrl == null) {
+					firebaseDatabaseUrl = System.getenv("FIREBASE_DATABASE_URL");
+				}
 
 				FirebaseOptions options = FirebaseOptions.builder()
 						.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-						.setDatabaseUrl(System.getenv("FIREBASE_DATABASE_URL")).build();
+						.setDatabaseUrl(firebaseDatabaseUrl).build();
 
 				FirebaseApp.initializeApp(options);
 			} catch (IOException e) {
