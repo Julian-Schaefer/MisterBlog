0694555933f77760ff8c340d876831cfd15ad0ef
Julian Schäfer <julian.david.schaefer@gmail.com>
2021-04-05 22:03:47 +0200

    Added Google Sign In

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,12 +29,46 @@ class App extends StatelessWidget {
 
                 // Once complete, show your application
                 if (snapshot.connectionState == ConnectionState.done) {
-                  return Center(child: Text("Connected"));
+                  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+                  return Center(
+                      child: Column(
+                    children: [
+                      MaterialButton(
+                          onPressed: () {
+                            signInWithGoogle();
+                          },
+                          child: Text("Sign In with Google")),
+                      Text((firebaseAuth.currentUser != null)
+                          ? firebaseAuth.currentUser!.displayName!
+                          : "Not logged in"),
+                    ],
+                  ));
                 }
 
                 // Otherwise, show something whilst waiting for initialization to complete
                 return Center(child: Text("Loading.."));
               },
             )));
+  }
+
+  void signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    if (googleUser == null) {
+      return;
+    }
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    // Create a new credential
+    final OAuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
