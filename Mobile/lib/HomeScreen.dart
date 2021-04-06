786dfea6922a0e558f4a6dd6041d575592d0ac10
Julian Schäfer <julian.david.schaefer@gmail.com>
2021-04-06 21:20:23 +0200

    Home and SignInScreen

---
+++ Mobile/lib/HomeScreen.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  final void Function() refreshAuthenticationState;
+
+  const HomeScreen({Key? key, required this.refreshAuthenticationState})
+      : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      children: [
+        Text((firebaseAuth.currentUser != null)
+            ? firebaseAuth.currentUser!.displayName!
+            : "Not logged in"),
+        MaterialButton(
+            onPressed: () async {
+              await signOut();
+            },
+            child: Text("Sign Out")),
+      ],
+    ));
+  }
+
+  Future<void> signOut() async {
+    await firebaseAuth.signOut();
+    widget.refreshAuthenticationState();
+  }
+}
