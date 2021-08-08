83544de8896bc0b7b661f5496bf0afaa3b6163b3
Julian Schäfer <julian.david.schaefer@gmail.com>
2021-08-09 00:18:36 +0200

    Handling Blog Selections

--- Flask/src/routes.py
+++ Flask/src/routes.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, request, jsonify
+from newspaper.article import ArticleException
+from newspaper import Article
+from flask import request, jsonify
+import newspaper
 
 from blog_selection import BlogSelection
 from database import db
@@ -6,26 +10,40 @@ from database import db
 routes = Blueprint('Routes', __name__)
 
 
-@routes.route('/asd')
-def index():
-    return "This is an example app"
+@routes.route("/blog-selection", methods=["POST"])
+def addBlogSelection():
+    userId = request.user.uid
+    if request.is_json:
+        data = request.get_json()
+        new_blog_selection = BlogSelection(
+            blogUrl=data['blogUrl'], userId=userId, isSelected=True)
+        db.session.add(new_blog_selection)
+        db.session.commit()
+        return {"message": f"Blog Selection {new_blog_selection.blogUrl} for User {new_blog_selection.userId} has been created successfully."}
+    else:
+        return {"error": "The request payload is not in JSON format"}
 
 
-@routes.route('/blog-selection', methods=['POST', 'GET'])
-def handle_cars():
+@routes.route('/selected', methods=['POST', 'GET'])
+def handleSelectedBlogs():
+    userId = request.user.uid
     if request.method == 'POST':
         if request.is_json:
             data = request.get_json()
-            new_blog_selection = BlogSelection(
-                blogUrl=data['blogUrl'], userId="Testi", isSelected=True)
-            db.session.add(new_blog_selection)
+            for blogSelection in data:
+                new_blog_selection = db.session.query(BlogSelection).get(
+                    (blogSelection['blogUrl'], userId))
+                if new_blog_selection:
+                    new_blog_selection.isSelected = blogSelection['isSelected']
+                    db.session.add(new_blog_selection)
             db.session.commit()
-            return {"message": f"Blog Selection {new_blog_selection.blogUrl} for User {new_blog_selection.userId} has been created successfully."}
+            return {"message": f"Updated selected Blogs successfully"}
         else:
             return {"error": "The request payload is not in JSON format"}
 
     elif request.method == 'GET':
-        blog_selections = BlogSelection.query.all()
+        blog_selections = db.session.query(
+            BlogSelection).filter_by(userId=userId)
         results = [
             {
                 "blogUrl": blog_selection.blogUrl,
@@ -33,3 +51,46 @@ def handle_cars():
             } for blog_selection in blog_selections]
 
         return jsonify(results)
+
+
+@routes.route("/posts")
+def getBlogPosts():
+    blog = newspaper.build("https://www.kauffmann.nl/",
+                           keep_article_html=True, fetch_images=False)
+
+    for article in blog.articles:
+        try:
+            article.download()
+            article.parse()
+            article.nlp()
+        except ArticleException as err:
+            print("Error downloading Article: {0}".format(err))
+
+    return jsonify([{
+        "title": article.title,
+        "authors": article.authors,
+        "summary": article.summary,
+        "content": article.article_html,
+        "date": article.publish_date
+    } for article in blog.articles])
+
+
+@routes.route("/post")
+def getBlogPostFromUrl():
+    url = request.args.get("url")
+    article = Article(url, keep_article_html=True)
+    article.download()
+    article.parse()
+    article.nlp()
+
+    # https://newspaper.readthedocs.io/en/latest/user_guide/advanced.html
+    article.clean_doc
+    article.clean_top_node
+
+    return {
+        "title": article.title,
+        "authors": article.authors,
+        "summary": article.summary,
+        "content": article.article_html,
+        "date": article.publish_date
+    }
