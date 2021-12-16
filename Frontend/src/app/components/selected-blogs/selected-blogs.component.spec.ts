3bf6c621c0c321fd1b36b6af73d1d24d03132cd8
Julian Schäfer <julian.david.schaefer@gmail.com>
2021-12-16 15:19:28 +0100

    Updated to Angular 11

--- Frontend/src/app/components/selected-blogs/selected-blogs.component.spec.ts
+++ Frontend/src/app/components/selected-blogs/selected-blogs.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SelectedBlogsComponent } from './selected-blogs.component';
 
@@ -6,7 +6,7 @@ describe('SelectedBlogsComponent', () => {
   let component: SelectedBlogsComponent;
   let fixture: ComponentFixture<SelectedBlogsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SelectedBlogsComponent ]
     })
