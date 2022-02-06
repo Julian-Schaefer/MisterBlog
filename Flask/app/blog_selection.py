62a426a89d07f28656154b8e864f785e482a9167
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-02-06 15:26:22 +0100

    Considering PageCounterURL and ignoring External Links

--- Flask/app/blog_selection.py
+++ Flask/app/blog_selection.py
@@ -9,12 +9,14 @@ class BlogSelection(db.Model):
     user_id = db.Column(db.String(), primary_key=True)
     is_selected = db.Column(db.Boolean())
     article_selectors = db.Column(JSONB())
+    page_counter_url = db.Column(db.String(), nullable=False)
 
-    def __init__(self, blog_url, user_id, is_selected, article_selectors):
+    def __init__(self, blog_url, user_id, is_selected, article_selectors, page_counter_url):
         self.blog_url = blog_url
         self.user_id = user_id
         self.is_selected = is_selected
         self.article_selectors = article_selectors
+        self.page_counter_url = page_counter_url
 
     def __repr__(self):
         return f"<Blog-Selection {self.blog_url},  {self.user_id}>"
