a03e28799f08e4dc9281e9028e7f4af4f4e41975
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-02-06 02:14:26 +0100

    Some good stuff

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request, jsonify
 from flask import request, jsonify
 from multiprocessing.dummy import Pool as ThreadPool
+import json
 
-from app.articleselector import get_articles, download_article, is_compatible
+from app.articleselector import get_article_selectors, get_articles, download_article
 from app.blog_selection import BlogSelection
 from app.database import db
 
@@ -11,39 +12,40 @@ bp = Blueprint('routes', __name__)
 
 @bp.route("/blog-selection", methods=["DELETE"])
 def deleteBlogSelection():
-    userId = request.user['user_id']
+    user_id = request.user['user_id']
     if request.is_json:
         data = request.get_json()
 
         db.session.query(
-            BlogSelection).filter_by(userId=userId, blogUrl=data['blogUrl']).delete()
+            BlogSelection).filter_by(user_id=user_id, blog_url=data['blogUrl']).delete()
         db.session.commit()
-        return {"message": f"Blog Selection {data['blogUrl']} for User {userId} has been deleted successfully."}
+        return {"message": f"Blog Selection {data['blogUrl']} for User {user_id} has been deleted successfully."}
     else:
         return {"error": "The request payload is not in JSON format"}
 
 
 @bp.route("/blog-selection", methods=["POST"])
 def addBlogSelection():
-    userId = request.user['user_id']
+    user_id = request.user['user_id']
     if request.is_json:
         data = request.get_json()
-        blogUrl = data['blogUrl']
-        if not blogUrl.startswith('https://'):
-            blogUrl = "https://" + blogUrl
+        blog_url = data['blogUrl']
+        if not blog_url.startswith('https://'):
+            blog_url = "https://" + blog_url
 
         blog_selection_exists = db.session.query(
-            BlogSelection).filter_by(userId=userId, blogUrl=blogUrl).one_or_none()
+            BlogSelection).filter_by(user_id=user_id, blog_url=blog_url).one_or_none()
 
         if blog_selection_exists:
             return {"error": "The specified Blog URL has already been added for this User."}, 409
 
-        if is_compatible(blogUrl):
+        article_selectors = get_article_selectors(blog_url)
+        if len(article_selectors) > 0:
             new_blog_selection = BlogSelection(
-                blogUrl=blogUrl, userId=userId, isSelected=True)
+                blog_url=blog_url, user_id=user_id, is_selected=True, article_selectors=json.dumps(article_selectors))
             db.session.add(new_blog_selection)
             db.session.commit()
-            return {"message": f"Blog Selection {new_blog_selection.blogUrl} for User {new_blog_selection.userId} has been created successfully."}
+            return {"message": f"Blog Selection {new_blog_selection.blog_url} for User {new_blog_selection.user_id} has been created successfully."}
         else:
             return {"error": "The provided Blog URL is not supported."}, 406
     else:
@@ -52,15 +54,15 @@ def addBlogSelection():
 
 @bp.route('/blog-selection/selected', methods=['POST', 'GET'])
 def handleSelectedBlogs():
-    userId = request.user['user_id']
+    user_id = request.user['user_id']
     if request.method == 'POST':
         if request.is_json:
             data = request.get_json()
             for blogSelection in data:
                 new_blog_selection = db.session.query(BlogSelection).get(
-                    (blogSelection['blogUrl'], userId))
+                    (blogSelection['blogUrl'], user_id))
                 if new_blog_selection:
-                    new_blog_selection.isSelected = blogSelection['isSelected']
+                    new_blog_selection.is_selected = blogSelection['isSelected']
                     db.session.add(new_blog_selection)
             db.session.commit()
             return {"message": f"Updated selected Blogs successfully"}
@@ -69,11 +71,11 @@ def handleSelectedBlogs():
 
     elif request.method == 'GET':
         blog_selections = db.session.query(
-            BlogSelection).filter_by(userId=userId)
+            BlogSelection).filter_by(user_id=user_id)
         results = [
             {
-                "blogUrl": blog_selection.blogUrl,
-                "isSelected": blog_selection.isSelected
+                "blogUrl": blog_selection.blog_url,
+                "isSelected": blog_selection.is_selected
             } for blog_selection in blog_selections]
 
         return jsonify(results)
@@ -81,17 +83,18 @@ def handleSelectedBlogs():
 
 @bp.route("/blog-selection", methods=["GET"])
 def getBlogPosts():
-    userId = request.user['user_id']
+    user_id = request.user['user_id']
     page = int(request.args.get('page'))
 
     blog_selections = db.session.query(
-        BlogSelection).filter_by(userId=userId, isSelected=True)
-
+        BlogSelection).filter_by(user_id=user_id, is_selected=True)
     articles = []
 
     if blog_selections.count() > 0:
         blog_selection_arguments = []
         for blog_selection in blog_selections:
+            blog_selection.article_selectors = json.loads(
+                blog_selection.article_selectors)
             blog_selection_arguments.append((page, blog_selection))
 
         pool = ThreadPool(len(blog_selection_arguments))
@@ -99,14 +102,22 @@ def getBlogPosts():
         pool.close()
         pool.join()
 
-        for (blogSelection, urls) in article_urls:
+        for (blog_selection, urls) in article_urls:
             pool = ThreadPool(len(article_urls))
-            blogArticles = pool.map(download_article, urls)
-            for article in blogArticles:
-                articles.append((blogSelection.blogUrl, article))
+            blog_articles = pool.map(download_article, urls)
+            for article in blog_articles:
+                articles.append((blog_selection.blog_url, article))
             pool.close()
             pool.join()
 
+    a = []
+    for article in articles:
+        if article[1].publish_date:
+            a += [article]
+        else:
+            print('asd')
+
+    articles = a
     articles.sort(key=lambda article: article[1].publish_date, reverse=True)
 
     return jsonify([{
@@ -115,9 +126,9 @@ def getBlogPosts():
         "authors": article.authors,
         "summary": article.summary,
         "content": article.article_html,
-        "blogUrl": blogUrl,
+        "blogUrl": blog_url,
         "postUrl": article.url
-    } for (blogUrl, article) in articles])
+    } for (blog_url, article) in articles])
 
 
 @bp.route("/blog-selection/post", methods=["GET"])
