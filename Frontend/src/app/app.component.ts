3506c3dc8740f6cd1eef050f2c91b65d4fd6a1fc
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-02-11 23:34:59 +0100

    Added Sign In Icons

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from './services/auth/auth.service';
 
@@ -10,10 +12,14 @@ import { AuthService } from './services/auth/auth.service';
 export class AppComponent {
   title = 'Blogify';
 
-  constructor(public authService: AuthService, private translateService: TranslateService) {
+  constructor(public authService: AuthService, private translateService: TranslateService,
+    private domSanitizer: DomSanitizer,
+    private matIconRegistry: MatIconRegistry) {
     translateService.addLangs(['de', 'en']);
     const defaultLang = this.translateService.getBrowserLang();
 
+    this.matIconRegistry.addSvgIcon("google_signin",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('./assets/svg/google_signin.svg'));
     this.translateService.setDefaultLang(defaultLang);
     this.translateService.use(defaultLang);
   }
