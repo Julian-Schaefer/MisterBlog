21c5e2a36c4ef480bfb5bda843f3ac87879b73cf
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-03-27 16:37:30 +0200

    Removed PreviewComponent

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PreviewComponent } from './components/preview/preview.component';
 import { SignInComponent } from './components/authentication/sign-in/sign-in.component';
 import { SignUpComponent } from './components/authentication/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './components/authentication/forgot-password/forgot-password.component';
@@ -14,7 +13,6 @@ import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent, canActivate: [AuthGuard] },
-  { path: 'preview', component: PreviewComponent, canActivate: [AuthGuard] },
   { path: 'post', component: PostComponent, canActivate: [AuthGuard] },
   // Unauthenticated
   { path: 'about', component: AboutComponent },
