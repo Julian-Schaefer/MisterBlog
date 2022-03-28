74060ae38472b20c9e68a2c171a1fd34fdc7a87b
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-03-28 22:36:22 +0200

    Forcing SSL

--- Frontend/server.js
+++ Frontend/server.js
@@ -1,18 +1,9 @@
 const express = require("express");
-
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (req.header('x-forwarded-proto') !== 'https') {
-        res.redirect(`https://${req.header('host')}${req.url}`);
-    } else {
-        next();
-    }
-}
+var forceSSL = require('force-ssl-heroku');
 
 const app = express();
-
+app.use(forceSSL);
 app.use(express.static("./dist/Mister-Blog"));
-app.use(requireHTTPS);
 
 app.get("/*", function (req, res) {
     res.sendFile("index.html", { root: "dist/Mister-Blog" });
