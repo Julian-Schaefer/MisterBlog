6cf5f99a55c0e177c040f4b5805c5e3e858cc6e6
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-04-06 23:00:41 +0200

    Added Public Guard

---
+++ Frontend/src/app/guards/public.guard.ts
@@ -0,0 +1,30 @@
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { AuthService } from '../services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublicGuard implements CanActivate {
+
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.authService.isLoggedIn) {
+        this.router.navigate(['posts']);
+      }
+    }
+
+    return true;
+  }
+
+}
