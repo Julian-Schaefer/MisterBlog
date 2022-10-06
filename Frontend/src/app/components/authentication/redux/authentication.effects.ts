a3fb648fa0d728096da6870b2381fae23b610293
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-10-06 22:54:00 +0200

    Added "Change Password" Section

--- Frontend/src/app/components/authentication/redux/authentication.effects.ts
+++ Frontend/src/app/components/authentication/redux/authentication.effects.ts
@@ -1,34 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { TranslateService } from '@ngx-translate/core';
-import { from, lastValueFrom } from 'rxjs';
+import { from } from 'rxjs';
 import { map, mergeMap, catchError } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import * as AuthenticationActions from './authentication.actions';
-import { NGXLogger } from "ngx-logger";
 
 @Injectable()
 export class AuthenticationEffects {
-
-    private async getErrorMessageFromError(error: any): Promise<string> {
-        this.logger.log(error);
-        let errorMessage = await lastValueFrom(this.translateService.get("error.unknown-error"));
-
-        if (error.name && error.name === "FirebaseError") {
-            if (error.code && error.code.startsWith("auth/")) {
-                const errorCode = "error.auth." + error.code.substring("auth/".length);
-                errorMessage = await lastValueFrom(this.translateService.get(errorCode));
-
-                if (errorMessage === errorCode) {
-                    this.logger.warn("No Translation found for error: ", errorCode, error.message);
-                    errorMessage = await lastValueFrom(this.translateService.get("error.unknown-error"));
-                }
-            }
-        }
-
-        return errorMessage;
-    }
-
 
     signInWithEmail$ = createEffect(() =>
         this.actions$.pipe(
@@ -36,7 +14,7 @@ export class AuthenticationEffects {
             mergeMap((action) => this.authService.signInWithEmail(action.email, action.password)
                 .pipe(
                     map(_ => AuthenticationActions.signInSuccess()),
-                    catchError(error => from(this.getErrorMessageFromError(error)).pipe(map(errorMessage =>
+                    catchError(error => from(this.authService.getErrorMessageFromError(error)).pipe(map(errorMessage =>
                         AuthenticationActions.signInFailed({ error: errorMessage })))
                     )
                 )
@@ -50,7 +28,7 @@ export class AuthenticationEffects {
             mergeMap((action) => this.authService.signUpWithEmail(action.email, action.password)
                 .pipe(
                     map(_ => AuthenticationActions.signUpWithEmailSuccess()),
-                    catchError(error => from(this.getErrorMessageFromError(error)).pipe(map(errorMessage =>
+                    catchError(error => from(this.authService.getErrorMessageFromError(error)).pipe(map(errorMessage =>
                         AuthenticationActions.signInFailed({ error: errorMessage })))
                     )
                 )
@@ -65,7 +43,7 @@ export class AuthenticationEffects {
                 return this.authService.signInWithProvider(action.provider)
                     .pipe(
                         map(_ => AuthenticationActions.signInSuccess()),
-                        catchError(error => from(this.getErrorMessageFromError(error)).pipe(map(errorMessage =>
+                        catchError(error => from(this.authService.getErrorMessageFromError(error)).pipe(map(errorMessage =>
                             AuthenticationActions.signInFailed({ error: errorMessage })))
                         ),
                     );
@@ -79,7 +57,7 @@ export class AuthenticationEffects {
             mergeMap((action) => this.authService.resetPassword(action.email)
                 .pipe(
                     map(_ => AuthenticationActions.resetPasswordSuccess()),
-                    catchError(error => from(this.getErrorMessageFromError(error)).pipe(map(errorMessage =>
+                    catchError(error => from(this.authService.getErrorMessageFromError(error)).pipe(map(errorMessage =>
                         AuthenticationActions.signInFailed({ error: errorMessage })))
                     )
                 )
@@ -93,7 +71,7 @@ export class AuthenticationEffects {
             mergeMap((_) => this.authService.sendVerificationEmail()
                 .pipe(
                     map(_ => AuthenticationActions.sendVerificationEmailSuccess()),
-                    catchError(error => from(this.getErrorMessageFromError(error)).pipe(map(errorMessage =>
+                    catchError(error => from(this.authService.getErrorMessageFromError(error)).pipe(map(errorMessage =>
                         AuthenticationActions.signInFailed({ error: errorMessage })))
                     )
                 )
@@ -101,11 +79,8 @@ export class AuthenticationEffects {
         )
     );
 
-
     constructor(
         private actions$: Actions,
-        private authService: AuthService,
-        private translateService: TranslateService,
-        private logger: NGXLogger
+        private authService: AuthService
     ) { }
 }
